3ab05ab03f29fa84c78eb67dae48234fc9270efb
Fabian Grünbichler <fabian.gruenbichler@student.tuwien.ac.at>
2014-10-20 20:21:06 +0200

    Avoid duplicate filename, add sample JavaDoc comments

--- onion-chainnode/src/main/java/at/ac/tuwien/aic/ws14/group2/onion/chain/HelloWorldChain.java
+++ onion-chainnode/src/main/java/at/ac/tuwien/aic/ws14/group2/onion/chain/HelloWorldChain.java
@@ -2,7 +2,14 @@ package at.ac.tuwien.aic.ws14.group2.onion.chain;
 
 import static at.ac.tuwien.aic.ws14.group2.onion.common.HelloLib.testPrint;
 
-public class HelloWorld {
+/**
+ * Sample class to test Gradle setup.
+ */
+public class HelloWorldChain {
+    /**
+     * Main method
+     * @param args CLI arguments
+     */
     public static void main(String [] args) {
         if(args == null || args.length == 0) {
             testPrint("No args");
