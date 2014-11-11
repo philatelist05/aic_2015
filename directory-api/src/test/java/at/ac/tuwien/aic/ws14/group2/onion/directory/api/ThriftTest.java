a3427a1573f6141b564b16e701afbd240560696d
Thomas <e0927710@student.tuwien.ac.at>
2014-11-11 23:37:12 +0100

    thrift ssl test

--- directory-api/src/test/java/at/ac/tuwien/aic/ws14/group2/onion/directory/api/ThriftTest.java
+++ directory-api/src/test/java/at/ac/tuwien/aic/ws14/group2/onion/directory/api/ThriftTest.java
@@ -6,10 +6,7 @@ import org.apache.thrift.protocol.TBinaryProtocol;
 import org.apache.thrift.protocol.TProtocol;
 import org.apache.thrift.server.TServer;
 import org.apache.thrift.server.TSimpleServer;
-import org.apache.thrift.transport.TServerSocket;
-import org.apache.thrift.transport.TServerTransport;
-import org.apache.thrift.transport.TSocket;
-import org.apache.thrift.transport.TTransport;
+import org.apache.thrift.transport.*;
 import org.junit.Test;
 
 import static org.junit.Assert.*;
@@ -56,6 +53,50 @@ public class ThriftTest {
 
         TTransport transport = new TSocket("localhost", THRIFT_PORT);
         transport.open();
+
+        TProtocol protocol = new TBinaryProtocol(transport);
+        DirectoryService.Client client = new DirectoryService.Client(protocol);
+
+        assertEquals(false, handler.called);
+        client.ping();
+        assertEquals(true, handler.called);
+
+        transport.close();
+
+        server.stop();
+        serverThread.join();
+    }
+
+    @Test
+    public void secure() throws TException, InterruptedException {
+
+        // Server Code
+
+        ServiceImpl handler = new ServiceImpl();
+        DirectoryService.Processor<DirectoryService.Iface> processor = new DirectoryService.Processor<>(handler);
+
+        TSSLTransportFactory.TSSLTransportParameters serverParams = new TSSLTransportFactory.TSSLTransportParameters();
+        serverParams.setKeyStore("keys/thrift-test.jks", "password");
+
+        TServerTransport serverTransport = TSSLTransportFactory.getServerSocket(9090, 0, null, serverParams);
+        final TServer server = new TSimpleServer(new TServer.Args(serverTransport).processor(processor));
+
+        Runnable serverMethod = () -> {
+            System.out.println("server started");
+            server.serve();
+            System.out.println("server finished");
+        };
+
+        Thread serverThread = new Thread(serverMethod);
+        serverThread.start();
+
+
+        // Client Code
+
+        TSSLTransportFactory.TSSLTransportParameters clientParams = new TSSLTransportFactory.TSSLTransportParameters();
+        clientParams.setTrustStore("keys/thrift-test.jks", "password");
+
+        TTransport transport = TSSLTransportFactory.getClientSocket("localhost", THRIFT_PORT, 0, clientParams);
 
         TProtocol protocol = new TBinaryProtocol(transport);
         DirectoryService.Client client = new DirectoryService.Client(protocol);
