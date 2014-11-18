e5b43c9ceaec220718fbefe3c7abc32ce98a3ed1
Fabian Grünbichler <fabian.gruenbichler@student.tuwien.ac.at>
2014-11-18 17:59:08 +0100

    add initial HeartBeatWorker (client side)

---
+++ node/common/src/main/java/at/ac/tuwien/aic/ws14/group2/onion/node/common/exceptions/DirectoryNotReachableException.java
@@ -0,0 +1,6 @@
+package at.ac.tuwien.aic.ws14.group2.onion.node.common.exceptions;
+
+public class DirectoryNotReachableException extends RuntimeException {
+    public DirectoryNotReachableException() {
+    }
+}
