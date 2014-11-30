225a09e83dcca8a8c127eb81449d041a5b88f8c2
Fabian Grünbichler <fabian.gruenbichler@student.tuwien.ac.at>
2014-11-30 13:30:04 +0100

    set DHKeyExchange in Circuit

--- node/local/src/main/java/at/ac/tuwien/aic/ws14/group2/onion/node/local/node/LocalNodeCore.java
+++ node/local/src/main/java/at/ac/tuwien/aic/ws14/group2/onion/node/local/node/LocalNodeCore.java
@@ -107,6 +107,8 @@ public class LocalNodeCore {
             return;
         }
 
+        circuit.setDHKeyExchange(keyExchange);
+
         EncryptedDHHalf encryptedDHHalf = new DHHalf(g, p, publicKey).encrypt(firstNode.getPublicKey());
         CreateCell cell = new CreateCell(circuit.getCircuitID(), firstNode.getEndPoint(), encryptedDHHalf);
         sendCell(cell, circuit, callBack);
