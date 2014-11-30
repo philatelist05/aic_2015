ad01c2b71dcd919120dc743ef587aa12a35af692
Stefan Gamerith <steff7@gmx.com>
2014-11-30 14:44:50 +0100

    Added structure for TargetWorker and xml configuration properties

--- node/common/src/main/java/at/ac/tuwien/aic/ws14/group2/onion/node/common/node/TargetWorker.java
+++ node/common/src/main/java/at/ac/tuwien/aic/ws14/group2/onion/node/common/node/TargetWorker.java
@@ -1,14 +1,33 @@
 package at.ac.tuwien.aic.ws14.group2.onion.node.common.node;
 
+import at.ac.tuwien.aic.ws14.group2.onion.node.common.cells.RelayCell;
+import at.ac.tuwien.aic.ws14.group2.onion.node.common.cells.RelayCellPayload;
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
+
 import java.io.IOException;
 
 /**
  * Created by Thomas on 22.11.2014.
  */
-public class TargetWorker implements AutoCloseable {
+public class TargetWorker implements AutoCloseable, Runnable {
+    static final Logger logger = LogManager.getLogger(TargetWorker.class.getName());
+
+    private final ConnectionWorker worker;
+    private final Endpoint endpoint;
 
     public TargetWorker(ConnectionWorker worker, Endpoint endpoint) {
-        // TODO
+        this.worker = worker;
+        this.endpoint = endpoint;
+    }
+
+    public void sendData(byte[] data) {
+
+    }
+
+    @Override
+    public void run() {
+//        worker.sendCell(new RelayCell(0, new RelayCellPayload(new byte[]{})));
     }
 
     @Override
