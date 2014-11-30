3fcb162230c4f05fdbd6bb1da7e2146527b559cc
Fabian Grünbichler <fabian.gruenbichler@student.tuwien.ac.at>
2014-11-30 23:59:23 +0100

    add toString

--- node/local/src/main/java/at/ac/tuwien/aic/ws14/group2/onion/node/local/node/ChainNodeMetaData.java
+++ node/local/src/main/java/at/ac/tuwien/aic/ws14/group2/onion/node/local/node/ChainNodeMetaData.java
@@ -14,6 +14,7 @@ import java.security.NoSuchProviderException;
 import java.security.PublicKey;
 import java.security.spec.InvalidKeySpecException;
 import java.security.spec.X509EncodedKeySpec;
+import java.util.Arrays;
 
 public class ChainNodeMetaData {
     static final Logger logger = LogManager.getLogger(ChainNodeMetaData.class.getName());
@@ -76,5 +77,14 @@ public class ChainNodeMetaData {
 
     public void setSessionKey(byte[] sessionKey) {
         this.sessionKey = sessionKey;
+    }
+
+    @Override
+    public String toString() {
+        return "ChainNodeMetaData{" +
+                "publicKey=" + publicKey +
+                ", endPoint=" + endPoint +
+                ", sessionKey=" + Arrays.toString(sessionKey) +
+                '}';
     }
 }
