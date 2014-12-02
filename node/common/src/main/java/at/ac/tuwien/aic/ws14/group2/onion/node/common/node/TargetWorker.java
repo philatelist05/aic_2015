89ff9588027ac308497dc145b07f64ce48e1d4cb
Stefan Gamerith <steff7@gmx.com>
2014-12-02 14:59:22 +0100

    Added Buffer to TargetWorker

--- node/common/src/main/java/at/ac/tuwien/aic/ws14/group2/onion/node/common/node/TargetWorker.java
+++ node/common/src/main/java/at/ac/tuwien/aic/ws14/group2/onion/node/common/node/TargetWorker.java
@@ -4,6 +4,8 @@ import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
 
 import java.io.IOException;
+import java.util.HashSet;
+import java.util.Set;
 
 /**
  * Created by Thomas on 22.11.2014.
@@ -13,14 +15,31 @@ public class TargetWorker implements AutoCloseable, Runnable {
 
     private final ConnectionWorker worker;
     private final Endpoint endpoint;
+    private final NoGapSkipListSet<Bucket> buffer;
 
     public TargetWorker(ConnectionWorker worker, Endpoint endpoint) {
         this.worker = worker;
         this.endpoint = endpoint;
+        this.buffer = new NoGapSkipListSet<>((b1, b2) -> Short.compare(b1.nr, b2.nr), this::allItemsInRange);
     }
 
     public void sendData(byte[] data, short sequenceNumber) {
+        Bucket bucket = new Bucket(data, sequenceNumber);
+        buffer.add(bucket);
+    }
 
+    private Set<Bucket> allItemsInRange(Bucket b1, Bucket b2) {
+        Set<Bucket> buckets = new HashSet<>();
+        if (b1.nr <= b2.nr) {
+            for (int i = b1.nr + 1; i < b2.nr; i++) {
+                buckets.add(new Bucket(new byte[]{}, (short)i));
+            }
+        } else {
+            for (int i = b1.nr - 1; i > b2.nr ; i--) {
+                buckets.add(new Bucket(new byte[]{}, (short)i));
+            }
+        }
+        return buckets;
     }
 
     @Override
@@ -31,5 +50,15 @@ public class TargetWorker implements AutoCloseable, Runnable {
     @Override
     public void close() throws IOException {
         // TODO
+    }
+
+    private class Bucket {
+        private byte[] data;
+        private short nr;
+
+        private Bucket(byte[] data, short sequenceNumber) {
+            this.data = data;
+            this.nr = sequenceNumber;
+        }
     }
 }
