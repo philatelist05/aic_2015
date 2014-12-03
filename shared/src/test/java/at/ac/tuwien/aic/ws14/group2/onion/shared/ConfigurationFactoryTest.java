2dc78ae87d613036ae5e53a0bb8082ebb365374e
Stefan Gamerith <steff7@gmx.com>
2014-12-03 18:09:12 +0100

    Added test skeleton for ConfigurationFactory

---
+++ shared/src/test/java/at/ac/tuwien/aic/ws14/group2/onion/shared/ConfigurationFactoryTest.java
@@ -0,0 +1,19 @@
+package at.ac.tuwien.aic.ws14.group2.onion.shared;
+
+import org.junit.Before;
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+public class ConfigurationFactoryTest {
+
+    @Before
+    public void setUp() throws Exception {
+
+    }
+
+    @Test
+    public void testGetConfiguration() throws Exception {
+
+    }
+}
