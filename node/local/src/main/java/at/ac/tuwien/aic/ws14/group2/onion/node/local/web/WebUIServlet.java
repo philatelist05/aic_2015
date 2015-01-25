2086e50fac200aa976c58f13ec181b416806e72b
Stefan Gamerith <steff7@gmx.com>
2015-01-25 16:00:00 +0100

    Added mustache to build

--- node/local/src/main/java/at/ac/tuwien/aic/ws14/group2/onion/node/local/web/WebUIServlet.java
+++ node/local/src/main/java/at/ac/tuwien/aic/ws14/group2/onion/node/local/web/WebUIServlet.java
@@ -1,11 +1,18 @@
 package at.ac.tuwien.aic.ws14.group2.onion.node.local.web;
 
+import com.github.mustachejava.DefaultMustacheFactory;
+import com.github.mustachejava.Mustache;
+import com.github.mustachejava.MustacheFactory;
+
 import javax.servlet.ServletException;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
 import java.io.PrintWriter;
+import java.io.StringReader;
+import java.io.Writer;
+import java.util.HashMap;
 
 /**
  * Created by Stefan on 23.01.15.
@@ -13,8 +20,15 @@ import java.io.PrintWriter;
 public class WebUIServlet extends HttpServlet {
     @Override
     protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
-        PrintWriter out = resp.getWriter();
-        out.println("Hello, world!");
-        out.close();
+        HashMap<String, Object> scopes = new HashMap<>();
+        scopes.put("name", "Mustache");
+        scopes.put("feature", "Perfect!");
+
+        MustacheFactory mf = new DefaultMustacheFactory();
+        Mustache mustache = mf.compile(new StringReader("{{name}}, {{feature}}!"), "example");
+        Writer wr = resp.getWriter();
+        mustache.execute(wr, scopes);
+        wr.flush();
+        wr.close();
     }
 }
